1a7719e836645931bd5d8c359e3ebe4bb82c2f39
Fr1end <friend_wolf2001@ukr.net>
2015-08-18 17:18:49 +0300

    change invitations_mailer

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -5,18 +5,24 @@ class InvitationsMailer < ActionMailer::Base
   def invitation_create(invitation)
     @invitation = invitation
     mail to: invitation.for_user.email,
+         body: "Вас запрошено вступити в команду #{invitation.to_team.name}",
+         content_type: "text/plain",
          subject: "Запрошення в команду"
   end
 
   def invitation_reject(invitation)
     @invitation = invitation
     mail to: invitation.to_team.captain.email,
+         body: "Користувач #{invitation.for_user.nickname} відмовився від запрошення",
+         content_type: "text/plain",
          subject: "Запрошення в команду відхилено"
   end
 
   def invitation_accept(invitation)
     @invitation = invitation
     mail to: invitation.to_team.captain.email,
+         body: "Користувач #{invitation.for_user.nickname} прийняв Ваше запрошення",
+         content_type: "text/plain",
          subject: "Запрошення в команду прийнято"
   end
 
