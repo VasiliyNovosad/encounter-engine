6a14f14b435035c02dc937a807185e209e4a1020
VasiliyNovosad <novosadvasiliy@gmail.com>
2015-11-26 11:15:56 +0200

    add postmark mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_option = { host: 'quest-racinglutsk.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'quest-racinglutsk.herokuapp.com' }
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
 
