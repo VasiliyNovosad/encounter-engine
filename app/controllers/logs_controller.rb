6e7d44baed93f44ee2e61e3079f0ef5941c4eaac
VasiliyNovosad <novosadvasiliy@gmail.com>
2016-04-04 22:52:59 +0300

    added short statistic

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,5 @@
 class LogsController < ApplicationController
-  before_action :authenticate_user!, :except [:show_short_log]
+  before_action :authenticate_user!, except: [:show_short_log]
   before_action :find_game
   before_action :ensure_author, only: [:show_live_channel, :show_level_log, :show_game_log]
   before_action :find_team, only: [:show_level_log, :show_game_log]
