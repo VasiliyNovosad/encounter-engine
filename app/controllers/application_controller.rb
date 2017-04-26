3614ab5548632e381df06426470791159cc8554f
illia Novosad <illiadren01@gmail.com>
2017-04-26 22:02:12 +0300

    devise permit parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_devise_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :nickname
-    devise_parameter_sanitizer.for(:account_update) << :nickname
-    devise_parameter_sanitizer.for(:account_update) << :phone_number
+    # devise_parameter_sanitizer.for(:sign_up) << :nickname
+    # devise_parameter_sanitizer.for(:account_update) << :nickname
+    # devise_parameter_sanitizer.for(:account_update) << :phone_number
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :phone_number])
   end
 
   def ensure_team_member
