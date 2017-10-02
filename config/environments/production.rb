69b47204b91a28fbeb798f722802c2eab4356f32
TanyaHusieva <tanya@eventsoft.com.ua>
2017-10-02 16:43:48 +0300

    changed email setting to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
-    # domain: 'quest-racinglutsk.herokuapp.com',
+    domain: 'quest-racinglutsk.herokuapp.com',
     address: 'smtp.gmail.com',
     port: 587,
     authentication: :plain,
