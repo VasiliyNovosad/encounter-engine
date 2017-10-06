9fa3c2840893ea9e117aa90455d3c495bd07b85f
Vasyl Novosad <novosad.vasiliy@gmail.com>
2017-10-06 21:04:03 +0300

    changed image uploading

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -2,15 +2,17 @@ class TinymceAssetsController < ApplicationController
   respond_to :json
 
   def create
-    geometry = Paperclip::Geometry.from_file params[:file]
-    image    = Image.create(image: params.permit(:file)[:file])# params.permit(:file))
-
-    render json: {
-      image: {
-        url:    image.image_url,
-        height: geometry.height.to_i,
-        width:  geometry.width.to_i
-      }
-    }, layout: false, content_type: 'text/html'
+    # geometry = Paperclip::Geometry.from_file params[:file]
+    # image    = Image.create(image: params.permit(:file)[:file])# params.permit(:file))
+    #
+    # render json: {
+    #   image: {
+    #     url:    image.image_url,
+    #     height: geometry.height.to_i,
+    #     width:  geometry.width.to_i
+    #   }
+    # }, layout: false, content_type: 'text/html'
+    upload = Cloudinary::Uploader.upload(params['file'])
+    render json: { image: { url: upload['url'] } }, content_type: 'text/html'
   end
 end
