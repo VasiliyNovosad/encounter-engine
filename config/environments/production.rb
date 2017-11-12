cf07b3b74315fc0419f91e706c927b66819cdda4
Vasyl Novosad <novosad.vasiliy@gmail.com>
2017-11-12 23:53:05 +0200

    fixed short game statistic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,12 +59,12 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: '95.46.98.220' }
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.GMAIL_DOMAIN }
   # config.action_mailer.register_interceptor(SendGrid::MailInterceptor)
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.secrets.GMAIL_USERNAME,
     password: Rails.application.secrets.GMAIL_PASSWORD,
-    domain: '95.46.98.220',
+    domain: Rails.application.secrets.GMAIL_DOMAIN,
     address: 'smtp.gmail.com',
     port: 587,
     authentication: :plain,
