0fe8645f6994ca0ae956c2b6c47d7ea3b630a191
Vasyl Novosad <novosad.vasiliy@gmail.com>
2017-11-12 23:25:45 +0200

    fixed short game statistic

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, '/home/deploy/encounter-engine'
 set :deploy_user, 'deploy'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.env', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.env', 'config/secrets.yml', 'config/cloudinary.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
