f0f5cab5032276729b69e303de41323f58c673f1
Vasyl Novosad <vasiliy@eventsoft.com.ua>
2017-11-23 17:34:03 +0200

    added redirect to quest.wtf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  match '*path' => redirect('https://quest.wtf')
+  match '*path' => redirect('https://quest.wtf'), via: [:get, :post, :put, :delete]
 
   # This line mounts Forem's routes at /forums by default.
   # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
