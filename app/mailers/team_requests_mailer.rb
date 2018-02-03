5c8d8e6ac2fa956e2481abc0d4f1ec54a416814e
Vasyl Novosad <novosad.vasiliy@gmail.com>
2018-02-03 23:52:10 +0200

    fixed mailers

--- app/mailers/team_requests_mailer.rb
+++ app/mailers/team_requests_mailer.rb
@@ -8,24 +8,24 @@ class TeamRequestsMailer < ActionMailer::Base
   def team_request_create(team_request)
     @team_request = team_request
     mail to: team_request.team.captain.email,
-         body: "Гравець #{team_request.user.nickname} хоче приєднатись до Вашої команди #{link_to(team_request.team.name, team_url(team_request.team))}",
+         body: "Гравець #{team_request.user.nickname} хоче приєднатись до Вашої команди #{team_url(team_request.team)}",
          content_type: "text/plain",
-         subject: "Запрошення в команду"
+         subject: "Запит на приєднання до складу команди"
   end
 
   def team_request_reject(team_request)
     @team_request = team_request
     mail to: team_request.team.captain.email,
-         body: "Вас не прийнято до складу команди #{link_to(team_request.team.name, team_url(team_request.team))}",
+         body: "Вас не прийнято до складу команди #{team_request.team.name} :#{team_url(team_request.team)}",
          content_type: "text/plain",
-         subject: "Запит на вступ до складу команди відхилено"
+         subject: "Запит на приєднання до складу команди відхилено"
   end
 
   def team_request_accept(team_request)
     @team_request = team_request
     mail to: team_request.team.captain.email,
-         body: "Вас прийнято до складу команди #{link_to(team_request.team.name, team_url(team_request.team))}",
+         body: "Вас прийнято до складу команди #{team_request.team.name} :#{team_url(team_request.team)}",
          content_type: "text/plain",
-         subject: "Запит на вступ до складу команди прийнято"
+         subject: "Запит на приєднання до складу команди прийнято"
   end
 end
