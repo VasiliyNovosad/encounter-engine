f8c7e48d7d3b9f5d5b2dec0c32327cd7ff2fb77b
Vasyl Novosad <vnovosad@corevalue.net>
2019-09-16 13:20:59 +0300

    fixing changing level order access

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,7 @@ class GamesController < ApplicationController
   before_action :find_team, only: [:show]
   before_action :ensure_author_if_game_is_draft, only: [:show]
   before_action :ensure_author_if_no_start_time, only: [:show]
-  before_action :ensure_author, only: [:edit, :update]
+  before_action :ensure_author, only: [:edit, :update, :new_level_order, :create_level_order]
   before_action :max_team_number_from_nz, only: [:update]
   before_action :ensure_author_if_no_finish_time, only: [:show_scenario]
   before_action :ensure_has_access, only: [:show_scenario]
