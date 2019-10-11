7820bece0b7efb7eab0cb605c810aef45a91ec54
Vasyl Novosad <vnovosad@corevalue.net>
2019-10-11 12:07:53 +0300

    fixing adding the same code for different teams

--- app/models/bonus_answer.rb
+++ app/models/bonus_answer.rb
@@ -6,7 +6,7 @@ class BonusAnswer < ActiveRecord::Base
 
   validates_presence_of :value, message: 'Не введено варіант коду'
 
-  validates_uniqueness_of :value, scope: [:bonus_id], message: 'Такий код вже є в даному бонусі'
+  validates_uniqueness_of :value, scope: %i[bonus_id team_id], message: 'Такий код вже є в даному бонусі'
 
   scope :of_bonus, ->(bonus_id) { where(bonus_id: bonus_id) }
   scope :of_team, ->(team_id) { where('team_id IS NULL OR team_id = ?', team_id) }
