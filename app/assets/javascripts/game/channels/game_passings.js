595e156ca29a4937470bfc4c03bfbc932119cfd6
Vasyl Novosad <vnovosad@corevalue.net>
2020-01-11 14:14:21 +0200

    fixed js error

--- app/assets/javascripts/game/channels/game_passings.js
+++ app/assets/javascripts/game/channels/game_passings.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  let game_info = $('#main-game-container');
+  var game_info = $('#main-game-container');
   App.game_passings = App.cable.subscriptions.create({
     channel: "GamePassingsChannel",
     game_passing_id: game_info.data('team-id'),
@@ -20,46 +20,46 @@ $(document).ready(function() {
         return;
       }
 
-      let hint = data.hint;
+      var hint = data.hint;
       if (hint) {
-        let hint_block = $('#penalty-hint-' + hint.id + ' .penalty-hint-body').get(0);
+        var hint_block = $('#penalty-hint-' + hint.id + ' .penalty-hint-body').get(0);
         hint_block.innerHTML = hint.text;
       }
 
-      let answers = data.answers || [];
-      for (let i = 0; i < answers.length; i++) {
+      var answers = data.answers || [];
+      for (var i = 0; i < answers.length; i++) {
         $('#entered-codes').prepend($('<p>' + answers[i].time + ' ' + answers[i].user + ' ' + answers[i].answer + '</p>'));
       }
-      let sectors = data.sectors || [];
-      for (let i = 0; i < sectors.length; i++) {
-        let sector = $('#sector-' + sectors[i].position).get(0);
+      var sectors = data.sectors || [];
+      for (var i = 0; i < sectors.length; i++) {
+        var sector = $('#sector-' + sectors[i].position).get(0);
         if (data.level.olymp) {
           sector.innerHTML = sectors[i].value;
         } else {
           sector.innerHTML = sectors[i].name + ': ' + sectors[i].value;
         }
       }
-      let bonuses = data.bonuses || [];
-      for (let i = 0; i < bonuses.length; i++) {
-        let elem = '<li id="bonus-' + bonuses[i].id + '">';
+      var bonuses = data.bonuses || [];
+      for (var i = 0; i < bonuses.length; i++) {
+        var elem = '<li id="bonus-' + bonuses[i].id + '">';
         elem += '<p class="bonus-answered"><strong>' + (bonuses[i].award != "" ? (bonuses[i].name + ': нагорода ' + bonuses[i].award) : (bonuses[i].name + ': виконано')) + ', код: ' + bonuses[i].value +'</strong></p>';
         if (bonuses[i].help) elem += bonuses[i].help;
         elem += '</li>';
         $('#bonus-' + bonuses[i].id).replaceWith(elem);
       }
-      let needed = data.needed || 0;
-      let closed = data.closed || 0;
+      var needed = data.needed || 0;
+      var closed = data.closed || 0;
       if (needed > 0 && closed > 0) {
         $('#codes-closed').get(0).innerHTML = 'Вірних кодів ' + closed + ' із ' + needed + ':';
       }
-      let input_lock = data.input_lock || {};
+      var input_lock = data.input_lock || {};
       if (input_lock.input_lock) {
         $('#answer').prop('readonly', true);
         $("#code_submit").attr("disabled", true);
 
-        let timer = Math.floor(input_lock.duration);
+        var timer = Math.floor(input_lock.duration);
 
-        let x = setInterval(function() {
+        var x = setInterval(function() {
 
           $('#entered-code').get(0).innerHTML = '<span class="wrong_code">блокування вводу: ' + --timer + ' c</span>';
 
@@ -74,7 +74,7 @@ $(document).ready(function() {
         $('#answer').prop('readonly', false);
       }
 
-      let timer_left = data.timer_left || 0;
+      var timer_left = data.timer_left || 0;
       if (timer_left !== 0) {
         LevelCompleter.setup({
           initialCountdownValue: timer_left,
@@ -102,14 +102,14 @@ $(document).ready(function() {
     },
 
     received: function(data) {
-      let input_lock = data.input_lock || {};
+      var input_lock = data.input_lock || {};
       if (input_lock.input_lock) {
         $('#answer').prop('readonly', true);
         $("#code_submit").attr("disabled", true);
 
-        let timer = Math.floor(input_lock.duration);
+        var timer = Math.floor(input_lock.duration);
 
-        let x = setInterval(function() {
+        var x = setInterval(function() {
 
           $('#entered-code').get(0).innerHTML = '<span class="wrong_code">блокування вводу: ' + --timer + ' c</span>';
 
