7d9920207c423ac54e735444d0a3d4764c16c872
Vasyl Novosad <vnovosad@corevalue.net>
2020-01-14 01:48:13 +0200

    rollback

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,10 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_cable.url = "ws://localhost:3334/cable"
-  config.action_cable.allowed_request_origins = ['http://localhost:3000']
+  #config.action_cable.url = "ws://localhost:3334/cable"
+  #config.action_cable.allowed_request_origins = ['http://localhost:3000']
+
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.url = "ws://localhost:3000/cable"
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end
