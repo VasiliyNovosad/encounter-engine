d7ead8c714e9c03f017a97a98cf7f7990b66582a
Vasyl Novosad <vnovosad@corevalue.net>
2020-05-23 16:52:23 +0300

    updated gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.13.0'
+lock '3.14.0'
 
 set :application, 'encounter-engine'
 set :repo_url, 'git@github.com:VasiliyNovosad/encounter-engine.git'
