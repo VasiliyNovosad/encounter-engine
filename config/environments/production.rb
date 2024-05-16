e31392bb2b65fb7ff03dcf7a49273b9af276b51f
Vasyl Novosad <vnovosad@corevalue.net>
2024-05-16 20:30:30 +0300

    update compressor for JavaScript assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :terser
+  config.tinymce.install = :copy
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
